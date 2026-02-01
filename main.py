880125340368c70d2675a0e554d05d3da161655f
Odyseo <shandler_s.e.a@hotmail.es>
2026-02-01 14:56:04 -0500

    Added a menu with new functions

--- main.py
+++ main.py
@@ -63,6 +63,55 @@ class Todo:
         for row in rows:
             print(row)
 
+    def change_priority(self, old_prio, new_prio):
+        self.c.execute(
+            'UPDATE tasks SET priority = ? WHERE id = ?',
+            (new_prio, old_prio)
+        )
+        self.conn.commit()
+
+    def delete_task(self, id):
+        self.c.execute(
+            'DELETE FROM tasks WHERE id = ?',
+            (id,)
+        )
+        self.conn.commit()
+    
+    def main_Menu(self):
+        print("1. Show Tasks\n"
+        "2. Add Task\n"
+        "3. Change Priority\n"
+        "4. Delete Task\n"
+        "5. Exit"
+        )
+
+        menu_selection = int(input('Enter menu option: '))
+
+        if menu_selection == 1:
+            print("\nTasts TO-DO:")
+            self.print_tasks()
+            print("-------------")
+            self.main_Menu()
+        elif menu_selection == 2:
+            self.add_task()
+            print("-------------")
+            self.main_Menu()
+        elif menu_selection == 3:
+            old_prio = int(input("Which priority level would you like to update?: "))
+            new_prio = int(input("What should the new priority level be?: "))
+            self.change_priority(old_prio, new_prio)
+            self.print_tasks()
+            print("-------------")
+            self.main_Menu()
+        elif menu_selection == 4:
+            del_id = int(input("Which ID would you like to delete?: "))
+            self.delete_task(del_id)
+            print("Task has been deleted")
+            print("-------------")
+            self.main_Menu()
+        else:
+            exit
+
 app = Todo()
-app.add_task()
-app.print_tasks()
+app.main_Menu()
+
